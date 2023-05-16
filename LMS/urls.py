7d2b865227296aff341f5c25521e23e1bf053dd5
Folorunso Timson <110051396+Timozino@users.noreply.github.com>
2023-05-16 11:42:32 +0100

    Update urls.py

--- LMS/urls.py
+++ LMS/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('base', views.BASE, name='base'),
     path('404', views.PAGE_NOT_FOUND, name = '404'),
     path('', views.HOME, name='home'),
