16a1ff3cbcf72e4c3c9349c8d808ed1dc4be460e
Folorunso Timson <110051396+Timozino@users.noreply.github.com>
2023-05-16 03:20:01 +0100

    Update settings.py

--- LMS/settings.py
+++ LMS/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qyt9*7**l^$m(n31!vd)7v!f(1_tbx9@of49lazv9qy**#isrm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
