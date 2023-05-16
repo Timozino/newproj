de8a80b41edccdb499bc25e14e9ccf1b519c6135
Folorunso Timson <110051396+Timozino@users.noreply.github.com>
2023-05-16 04:44:48 +0100

    Update settings.py

--- LMS/settings.py
+++ LMS/settings.py
@@ -146,7 +146,7 @@ LOGOUT_REDIRECT_URL = 'login'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'timsonfoli@gmail.com'
