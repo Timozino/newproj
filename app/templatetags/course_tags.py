3a25206bc1a451b0a0bc914c0c9f87e1a3aba52c
Folorunso Timson <110051396+Timozino@users.noreply.github.com>
2023-05-16 02:30:44 +0100

    Update course_tags.py

--- app/templatetags/course_tags.py
+++ app/templatetags/course_tags.py
@@ -9,7 +9,7 @@ register = template.Library()
 
 @register.simple_tag
 def discount_calculation(price,discount):
-    if discount is None or discount is 0:
+    if discount is None or discount == 0:
         return price
     sellprice = price
     sellprice = price - (price * discount/100)
