f4a2d370f0236973a8fe60366d5bdd9ed23cfff3
Folorunso Timson <110051396+Timozino@users.noreply.github.com>
2023-05-16 04:20:56 +0100

    Update settings.py

--- LMS/settings.py
+++ LMS/settings.py
@@ -126,12 +126,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'Static/'
 
-STATIC_ROOT ='staticfiles/'
+STATIC_ROOT ='/staticfiles/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'Static')
 ]
 
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
